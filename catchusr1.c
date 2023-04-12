2bca9a08db2138417e4fd5a68cc83566bad45f61
Freddy Lopez <freddlopez01@gmail.com>
2023-04-12 00:51:16 -0700

    written during lab

--- catchusr1.c
+++ catchusr1.c
@@ -1,8 +1,26 @@
 #include "p1fxns.h"
 #include <stdlib.h>
 #define UNUSED __attribute__((unused))
+
+volatile bool USR1_seen = false; 
+
+void onusr1(UNUSED int sig){
+	USR1_seen = true; 
+
+}
 ​
 int main(UNUSED int argc, UNUSED char *argv[]) {
-	
+	struct timespec ms20 = {0, 20000000};
+	if (signal(SIGUSR1, onusr1) == SIG_ERROR){
+		printf(stderr, "Cant establish the SIGUSR1 handler\n");
+		return EXIT_FAILURE;
+	}
+
+
+	while(!USR1_seen){
+		(void)nanosleep(&ms20, NULL);
+	}
+
+	printf("SIGUSR1 recieved\n");
 	return EXIT_SUCCESS;
 }
--- getword.c
+++ getword.c
@@ -3,6 +3,20 @@
 #define UNUSED __attribute__((unused))
 ​
 int main(UNUSED int argc, UNUSED char *argv[]) {
+	char buf[4096], word[4096];
+	while(p1getline(0, buf, sizeof(buf)) != 0){
+		int i = p1strchr(buf, "\n");
+		buf[i] = "\0";
+
+		i = 0; 
+		p1putstr(1, '|');
+		while((i = p1getword(buf, i, word)) != -1){
+			p1putstr(1, word);
+			p1putstr(1, '|');
+		}
+		p1putstr(1, "\n");
+
+	}
 	
 	return EXIT_SUCCESS;
 }
